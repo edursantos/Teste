cbadddbd7757799c49c11174864f1db6dcfb9c94
Eduardo <eduardo.ramos@discente.univasf.edu.br>
2020-04-24 16:40:45 -0300

    6

---
+++ ex007.py
@@ -0,0 +1,5 @@
+n1=int(input('Qual é a altura dessa parede? (Metros)'))
+n2=int(input('Qual é a largura dessa parede? (Metros'))
+n3= n1*n2
+n4=n3/2
+print('Você precisará de {} litros de tinta para pintar uma área de {}m²'.format(n4,n3))
